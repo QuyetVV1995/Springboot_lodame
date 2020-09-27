54d031cfe7dd51bff30c8f1d47c2ccd8683542ad
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-09-27 19:58:40 +0700

    #11: Spring Boot JPA + MySQL

---
+++ src/main/java/com/example/demo/Lesson11_JPA/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.Lesson11_JPA;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Mapping thông tin biến với tên cột trong Database
+    @Column(name = "hp")
+    private int hp;
+    @Column(name = "stamina")
+    private int stamina;
+
+    // Nếu không đánh dấu @Column thì sẽ mapping tự động theo tên biến
+    private int atk;
+    private int def;
+    private int agi;
+}
