08d2eb8bfd5595f1d48df7aa12073cfd504b3653
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-09-27 16:17:09 +0700

    #9:  Giải thích cách Thymeleaf vận hành + Expression + Demo Full

--- src/main/java/com/example/demo/Lesson8/WebController.java
+++ src/main/java/com/example/demo/Lesson8/WebController.java
@@ -6,32 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Đánh dấu đây là một Controller
 // Nơi tiếp nhận các reqquest từ phía người dùng
 @Controller
 public class WebController {
 
     // Đón nhận request GET
-    @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
+    @GetMapping("") // Nếu người dùng request tới địa chỉ "/"
     public String index() {
         return "index"; // Trả về file index.html
     }
 
-    @GetMapping("/about")
-    public String about(){
-        return  "about";
+    @GetMapping("/profile")
+    public String profile(Model model){
+        // Tạo ra thông tin
+        List<Info> profile = new ArrayList<>();
+        profile.add(new Info("fullname", "Vu Van Quyet"));
+        profile.add(new Info("nickname", "QuyetVV"));
+        profile.add(new Info("gmail", "quyeta2ubqn@gmail.com"));
+        profile.add(new Info("facebook", "https://www.facebook.com/vu.vanquyet.50/"));
+        profile.add(new Info("github", "https://github.com/QuyetVV1995"));
+
+        // Đưa thông tin vào Model
+        model.addAttribute("QuyetVVProfile", profile);
+
+        // TRả về template profile.html
+        return "profile";
     }
 
-    @GetMapping("/hello")
-    public String hello(
-            // Request param "name" sẽ được gán giá trị vào biến String
-            @RequestParam(name = "name", required = false, defaultValue = "") String name,
-            // Model là một object của Spring Boot, được gắn vào trong mọi request.
-             Model model)
-    {
-        // Gắn vào model giá trị name nhận được
-        model.addAttribute("name", name);
-
-        return "hello"; // trả về file hello.html cùng với thông tin trong object Model
-    }
 }
